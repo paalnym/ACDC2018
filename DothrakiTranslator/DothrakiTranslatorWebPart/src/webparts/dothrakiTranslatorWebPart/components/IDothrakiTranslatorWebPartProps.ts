40cabb139e91934d458b6031dc135795a0b8327a
paalnym <pal.nymoen@bouvet.no>
2018-02-08 14:52:06 +0100

    commit

---
+++ DothrakiTranslator/DothrakiTranslatorWebPart/src/webparts/dothrakiTranslatorWebPart/components/IDothrakiTranslatorWebPartProps.ts
@@ -0,0 +1,3 @@
+export interface IDothrakiTranslatorWebPartProps {
+  description: string;
+}
---
+++ DothrakiTranslator/DothrakiTranslatorWebPart/src/webparts/dothrakiTranslatorWebPart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IDothrakiTranslatorWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'DothrakiTranslatorWebPartWebPartStrings' {
+  const strings: IDothrakiTranslatorWebPartWebPartStrings;
+  export = strings;
+}
